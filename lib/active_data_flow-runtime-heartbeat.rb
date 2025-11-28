f3d452cea2be7ae365cb5aaacfa99df187a45b15
Eric Laquer <LaquerEric@gmail.com>
2025-11-28 08:29:59 -0500

    Update 2025-11-28 08:29:59

--- lib/active_data_flow-runtime-heartbeat.rb
+++ lib/active_data_flow-runtime-heartbeat.rb
@@ -6,5 +6,4 @@ require 'active_data_flow'
 
 require_relative 'active_data_flow/runtime/heartbeat/version'
 require_relative 'active_data_flow/runtime/heartbeat/configuration'
-require_relative 'active_data_flow/runtime/heartbeat/runner'
 require_relative 'active_data_flow/runtime/heartbeat/flow_executor'
