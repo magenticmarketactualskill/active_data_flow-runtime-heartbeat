f40addc4e344cfb091c093f37f948cc44bd91efa
Eric Laquer <LaquerEric@gmail.com>
2025-11-30 11:53:28 -0500

    Update 2025-11-30 11:53:28

--- lib/active_data_flow/runtime/heartbeat/base.rb
+++ lib/active_data_flow/runtime/heartbeat/base.rb
@@ -3,7 +3,13 @@ module ActiveDataFlow
     module Runtime
         module Heartbeat
             class Base
-                attr_accessor :name, :source, :sink, :runtime
+                attr_accessor :name, :source, :sink, :runtime, :message_id_calc
+                
+                def initialize(options = {})
+                    @message_id_calc = options[:message_id_calc] || lambda { |message| message['id'] }
+                    @transform_collision = options[:transform_collision] || false
+
+                end
             end
         end
     end
